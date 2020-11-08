9dd855c455138f46967ca568a4db587ef29376c9
Anthocerophyta <love.tom.100.mag@gmail.com>
2020-11-08 20:00:36 +0700

    Update _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,24 @@ import 'bootstrap/scss/bootstrap.scss'
 import '../styles/home.css'
 import '../styles/dashboard.css'
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <div className="d-flex">
-        <Component {...pageProps} />
-      </div>
-    )
-  }
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className="d-flex">
+      <Component {...pageProps} />
+    </div>
+  )
 }
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
 
 export default MyApp
